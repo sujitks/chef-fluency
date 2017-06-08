2335f7f0f8325503528c4509fa798276af662f2f
sujitks <sujit.singh@gmail.com>
2017-06-08 21:20:11 +0100

    Create default.rb

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+package 'apache2' do
+    package_name 'httpd'
+    action :install
+end
+
+service 'apache2' do
+    service_name 'httpd'
+    action[:start, :enable]
+end
